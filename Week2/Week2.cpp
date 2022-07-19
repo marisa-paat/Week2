34bd2968b6db9584f5e7cec84c328395a6c5a14d
marisa-paat <109453353+marisa-paat@users.noreply.github.com>
2022-07-19 14:37:29 -0700

    Add project files.

---
+++ Week2/Week2.cpp
@@ -0,0 +1,32 @@
+//find sum and average of 3 inputs
+//output cube of number
+#include <iostream>
+using namespace std;
+
+float findCube(float n) {
+    return n * n * n;
+}
+
+int main()
+{
+    float cube;
+
+    int a;
+    cout << "Enter number 1 : ";
+    cin >> a;
+
+    int b;
+    cout << "Enter number 2 : ";
+    cin >> b;
+
+    int c;
+    cout << "Rnter number 3 : ";
+    cin >> c;
+
+    cube = findCube(a);
+
+    int sum = a + b + c;
+    int average = (a + b + c) / 3;
+    cout << "Cube of " << a << " is: " << cube << endl;
+    cout << "Average = " << average << "and Sum = " << sum << endl;
+}
